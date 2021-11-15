e124fe88c2b9fd7f435c5a4e5df68d55607a6a75
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-15 15:36:45 +0300

    file module

--- firstSection/appC.js
+++ firstSection/appC.js
@@ -1,6 +1,7 @@
 const appB = require('./appB');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 console.log(appB.height);
 appB.log(123);
@@ -17,7 +18,7 @@ console.log("dirname ",__dirname); //Çalıştığımız dosyanın klasör yolun
  * Http Module
  */
 
-/**path modulü */
+/**path module */
 
 let result = path.resolve('app.js');
 console.log("result",result);
@@ -41,3 +42,59 @@ console.log("response",response);
 console.log("hostname",response.hostname);
 console.log("query",response.query.year);
 
+
+/**File system module */
+/*Read files in the dir */
+const files = fs.readdir('./',function(error,data){
+    if(error){
+        console.log("error",error);
+    }else{
+        console.log("data",data);
+    }
+});
+
+/**Read files */
+const data = fs.readFile('index.html','utf-8',function(error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("data",data);
+    }
+})
+
+/**write file */
+/**İf file exists ,It deletes file content and adds new content. */
+fs.writeFile('deneme.txt','Hello nodejs.. :)',function(error,data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('file created')
+    }
+})
+
+/**Append file */
+/**If file exists ,It adds new content directly and  does not delete the old one*/
+fs.appendFile('deneme1.txt','Hellooooo1',function(error,data){
+    if(error){
+        console.log("error",error);
+    }else{
+        console.log("file append");
+    }
+})
+
+/**Delete file */
+fs.unlink('deneme.txt',function(error,data){
+    if(error){
+        console.log("error",error);
+    }else{
+        console.log("File deleted");
+    }
+})
+
+/**Rename file */
+
+fs.rename('deneme1.txt','example.txt',function(error,data){
+    if(error){
+        console.log("error",error);
+    }
+})
