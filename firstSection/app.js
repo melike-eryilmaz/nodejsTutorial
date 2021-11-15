77708872c51e25892016297e07a4884da059968c
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-15 11:15:08 +0300

    global object

---
+++ firstSection/app.js
@@ -0,0 +1,17 @@
+//browser
+var firstName = 'Melike';
+
+console.log(firstName);
+//console.log(window);
+
+//window.console.log("window console");
+//window.setTimeout   ; window objesinin bu gibi func ları vardır.
+
+//nodejs
+
+var age = 24;
+
+console.log("global",global);
+console.log("global.age",global.age);
+
+//global objesininde func ları vardır.
---
+++ firstSection/appA.js
@@ -0,0 +1,7 @@
+/**
+ * Bir html de birden fazla script dosyası verilebilir.
+ * Bu dosyalarda tanımlanan değişkenler browser da window global objesi altında tutulur.
+ */
+var lastName = 'Eryilmaz';
+
+
