f05a875ccc948b1f2aea2fbeff353dd2dc84a7f1
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-18 11:03:06 +0300

    routing

--- expressApp/app.js
+++ expressApp/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+/*
 //Herhangi bir url belirtmedik .Tüm url lerde çalışır.
 app.use((req,res,next)=>{
     console.log("middleware 1 çalıştırıldı.");
@@ -13,6 +13,35 @@ app.use((req,res,next)=>{
     console.log("middleware 2 çalıştırıldı.");
     res.send('<h1>helloo from middleware</h1>')
 })
+*/
+
+/*
+//Aşağıdaki gibi / ile başlayan route 'u en başa koyarsak hepsi / ile başladığı için hep aynı response döner.
+// '/' karşılık gelen response döner.
+app.use('/',(req,res,next)=>{
+    res.send('ana route')
+})
+
+app.use('/app-product-delete',(req,res,next)=>{
+    res.send('product delete')
+})
+
+*/
+
+//Örneğin her ana dizine gelindiğinde loglama yapmak istiyoruz.Aşağıdaki gibi yaparız.
+app.use('/',(req,res,next)=>{
+    console.log("Loglama yapılıyor..");
+    next();
+})
+
+app.use('/app-product-delete',(req,res,next)=>{
+    res.send('product delete')
+})
+
+
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('Welcome home page')//default text/html gönderir.
