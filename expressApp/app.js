196efcfc9cc93b45d20ccc7f693a337823c2cf4b
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-18 10:44:43 +0300

    middleware

--- expressApp/app.js
+++ expressApp/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 
+//Herhangi bir url belirtmedik .Tüm url lerde çalışır.
+app.use((req,res,next)=>{
+    console.log("middleware 1 çalıştırıldı.");
+    next();
+
+})
+
+app.use((req,res,next)=>{
+    console.log("middleware 2 çalıştırıldı.");
+    res.send('<h1>helloo from middleware</h1>')
+})
+
 app.get('/',(req,res)=>{
     res.send('Welcome home page')//default text/html gönderir.
 
