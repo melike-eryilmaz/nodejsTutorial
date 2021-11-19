eeec0e4a0a4a919c529ea96bbc0ecfcd2cf90a5d
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-19 15:46:32 +0300

    dynamic contents

--- templateEngineApp/app.js
+++ templateEngineApp/app.js
@@ -12,15 +12,22 @@ app.set('view engine','pug');
 app.set('views','./views');
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    const products = [
+        {name:'İphone 11',price:'100$',description:'Good phone',image:'1.jpg'},
+        {name:'İphone 12',price:'500$',description:'Good phone',image:'2.jpg'},
+        {name:'İphone 13',price:'700$',description:'Good phone',image:'3.jpg'},
+        {name:'İphone 14',price:'400$',description:'Good phone',image:'4.jpg'}
+
+];
+    res.render('index',{title:'Products',products:products});
 })
 
 app.get('/add-product',(req,res)=>{
-    res.render('add-product');
+    res.render('add-product',{title:'Add a new product'});
 })
 
 app.use((req,res)=>{
-    res.render('404');
+    res.render('404',{title:'404'});
 })
 
 app.listen(3000,()=>{
